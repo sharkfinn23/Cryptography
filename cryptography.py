a55dfa7912c21a0b253e96113528a1af73b1b76f
sharkfinn23 <thomashackett@hanovernorwichschools.org>
2016-10-11 15:29:30 -0400

    Updated from Brython Server: 10/11/2016 3:29:30 PM

--- cryptography.py
+++ cryptography.py
@@ -14,12 +14,18 @@ code=str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
 while code not in ["e","d","q"]:
     print("Did not understand command, try again.")
     code=str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
-if code == "e":
+if code == "q":
+    print("Goodbye! ")
+while code == "e":
+    key=list(input("Key: ")
     message=input("Message: ")
     for x in range (0, len(message)):
         message[x]=associations.find(message[x])
+    for x in range (0, len(key)):
+        key[x]=associations.find(key[x])
+    while len(key)<len(message)
+    
 if code == "d":
     key=input("Key: ")
-if code == "q":
-    print("Goodbye! ")
+
 
